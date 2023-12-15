27246a2e5338143c44b484150458fbf5a5980012
Atasik <Atasikgames@gmail.com>
2023-12-15 09:21:04 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -5,16 +5,15 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-const addCard = (name, link, deleteFunction) => {
+const createCard = (name, link, deleteFunction) => {
     const card = cardTemplate.querySelector('.card').cloneNode(true);
-    card.querySelector('.card__image').setAttribute('src', link);
-    const cardDescription = card.querySelector('.card__description');
-    cardDescription.querySelector('.card__title').textContent = name;
-
     const deleteButton = card.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', () => {  
         deleteFunction(card);
     });
+    card.querySelector('.card__image').setAttribute('src', link);
+    card.querySelector('.card__image').setAttribute('alt', name);
+    card.querySelector('.card__title').textContent = name;
 
     return card;
 };
@@ -27,7 +26,7 @@ const deleteCard = (card) => {
 // @todo: Вывести карточки на страницу
 const renderCards = (cards) => {
     cards.forEach((item) => {
-        placesList.append(addCard(item.name, item.link, deleteCard));
+        placesList.append(createCard(item.name, item.link, deleteCard));
     });
 };
 
