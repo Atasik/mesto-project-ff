af2557b09f9bb705134c0e8d375d93d8cc83c496
Atasik <Atasikgames@gmail.com>
2023-12-17 06:23:18 +0300

    ultra fix

--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,6 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
-        // generator: {
-        //   filename: 'images/[name][ext]',
-        // },
       },
       {
         test: /\.css$/,
